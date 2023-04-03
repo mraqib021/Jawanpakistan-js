f080556ab95f5298214add79642a9d8fc378a9d3
mraqib021 <mraqib021@gmail.com>
2023-04-03 06:31:28 +0500

    1 Assignment Js

---
+++ 1 Assignment Practice/app.js
@@ -0,0 +1,36 @@
+// alert("Hello World");
+
+var Message = "Hello World";
+
+console.log(Message);
+
+var Name = prompt("Enter Your Name");
+
+alert("Hello " + Name +  " !");
+
+var num1 = "250";
+var num2 = "19";
+
+// Concatination
+
+var total = num1 + num2;
+
+console.log(total);
+
+// Substraction
+
+var total1 = num1 - num2;
+
+console.log(total1);
+
+// If Else
+
+var Signal = true;
+// var Signal = false;
+
+if (Signal) {
+  alert("Signal is Green ! \nGoo");
+} else {
+  alert("Signal is Red ! \nstop");
+}
+
