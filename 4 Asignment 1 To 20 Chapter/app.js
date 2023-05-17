a799fd4da29faef8ba9949b2c957635798186114
mraqib021 <mraqib021@gmail.com>
2023-05-17 08:31:26 +0500

    4 Assignment

---
+++ 4 Asignment 1 To 20 Chapter/app.js
@@ -0,0 +1,352 @@
+// // Chapter 1 Alerts
+
+// // Q No 1
+
+alert("First Name : Aqib");
+alert("Last Name : Iqbal");
+alert("Email : mraqib021@gmail.com");
+alert("Mobile Number : 03022114262");
+alert("Password : *******");
+
+// // Q No 2
+
+alert("I am Learning Javascript !");
+
+// // Q No 3
+
+alert("Heloo World !");
+
+// // Chapter 2 Variable For String
+
+// // Q No 1
+
+var firstName;
+
+// // Q No 2
+
+var Name;
+
+Name = "Aqib";
+
+// // Q No 3
+
+var teamName = "3 Idiots";
+
+alert("Team Name : " + teamName);
+
+// // Q No 4
+
+var bestMan = "Charlie";
+
+bestMan = "Aqib";
+
+//  // Chapter 3 Variables for Numbers
+
+// // Q No 1
+
+var caseQty ;
+
+// // Q No 2
+
+caseQty = 144;
+
+// // Q No 3
+
+var num = 9;
+
+// // Q No 4
+
+var sum ;
+
+sum = 2 + 5;
+
+// // Q No 5
+
+var price = 100;
+var shippingCharge = 10;
+var orderTotal = price + shippingCharge;
+alert("Total Amount : " + orderTotal);
+
+// // Q No 6
+
+var num1 = 12;
+num1 = num1 + 5;
+alert(num1);
+
+// // Chapter No 4 Variable Name Legal & Illegal
+
+// // Q No 1
+
+var productCost= 3.45;
+
+// // Q No 2
+
+var nameOfBrand;
+
+// // Q No 3
+
+var anyNumber = 16;
+
+// // Q No 4
+
+var firstName ;
+var lastName ;
+
+// // Q No 5
+
+// // Legal
+
+var legal ;
+var camelCase ;
+var NAME ;
+var $dads ;
+var num123 ;
+
+// // Illegal
+
+// var %2321 ;
+// var 1num ;
+// var #!21 ;
+// var !name ;
+// var ?name ;;
+
+// // Chapter 5 (Math Expression I)
+
+// // Q No 1
+
+var name = "Modulus"
+
+// // // Q No 2
+
+console.log("2");
+
+// // Q No 3
+
+var largeNum = 2000000;
+alert(largeNum)
+
+// // Q No 4
+
+var num1 = 25 - 12;
+var num2 = num1;
+alert(num2)
+
+// // Q No 5
+
+// Pending
+
+// // Q No 6
+
+alert(2 * 6);
+
+// // Chapter 6 (Math Expression II)
+
+// // Q No 1
+
+var x = 23 - 1;
+var y = 12 * 2;
+console.log(x);
+console.log(y);
+
+// // Q No 2
+
+var x = 100 ;
+x = x--;
+
+// Q No 3
+
+console.log("Y : " + 51)
+
+// Q No 4
+
+console.log("Z : " + 49);
+
+// // Q No 5
+
+var num = 200;
+var newNum = num;
+num = num--;
+
+// // Q No 6
+
+var num = 1;
+var newNum = num;
+
+// // Q No 7
+
+var x = 20;
+x = x++;
+alert(x);
+
+// Chapter 7 Math Expression III
+
+// Q No 1
+
+console.log("The Value of Calculated Num :" + " 14");
+
+// // Q No 2
+
+console.log("The Value of Calculated Num :" + " 24");
+
+// // Q No 3
+
+console.log("The Value of Calculated Num :" + " 24");
+
+// // Q No 4
+
+console.log("The Value of Calculated Num :" + " 18");
+
+// // Q No 5
+
+var cost = (2 + 2) * (4 + 10);
+
+console.log(cost);
+
+// // Q No 6
+
+var units = 2 + 2 * 4 + 10;
+
+console.log(units);
+
+// // Q No 7
+
+var pressure = 4 / 2 * 2;
+
+console.log("Pending");
+
+// // Chapter No 8 Concatenating Text Strings
+
+// // Q No 1
+
+console.log("22");
+
+// // Q No 2
+
+alert("Hello,Dolly");
+
+// // Q No 3
+
+alert("333");
+
+// // Q No 4
+
+var a = "Pakistan";
+var b = "Zindabad";
+alert(a + b);
+
+// // Q No 5
+
+var Concatenation = "35" + 2;
+
+// // Q No 6
+
+var firstName = "Aqib";
+var lastName = "Iqbal";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+// Chapter No 9 Prompts
+
+ // Q No 1
+
+var response = prompt("Enter Your First Name");
+var firstName = response;
+console.log("first Name : " + firstName);
+
+// // Q No 2
+
+var response = prompt("Enter Your Country Name", "China");
+var country = response;
+console.log("Country : " + country);
+
+// // Q No 3
+
+var yourName = prompt("Enter Your Name");
+
+// // Q No 4
+
+var msg = prompt("Enter Your Message", "Hello");
+
+// // Q No 5
+
+var one = prompt("Message ?", "Hello");
+var two = one;
+
+// // Q No 6
+
+var response = prompt(
+  "Enter Your Favourite Programming Language",
+  "JavaScript"
+);
+alert(response);
+
+// // Chapter 10 If Statement
+
+// // Q No 1
+
+var city = "karachi";
+if (city == "karachi") {
+  console.log("The City Of Lights");
+}
+
+// // Q No 2
+
+var x = 0;
+var y = 0;
+if(x === y){
+   var z = prompt("What's the Value of 'Z'")
+   var a = z;
+}
+
+// // Q No 3
+
+var zipCode = 10010;
+if(zipCode == "10011"){
+    alert("Karachi")
+}else(
+    alert("Please Write Correct City ! ")
+)
+
+// Q No 4
+
+
+// Pending
+
+// Chapter  Comparison Operators
+
+// // Q No 1
+
+var x = 0;
+var y = 1;
+if(x != y){
+    alert("X Not Equal Y")
+};
+
+// // Q No 2
+
+if(y >= x){
+    console.log("X is Equal to : " + x);
+}
+
+// // Q No 3
+
+if(x != 12){
+    x = 12
+    console.log(x)
+}
+
+// // Q No 4
+
+if(20 != 15){
+    alert("Congrulations")
+}
+
+// // Q No 5
+
+var name = prompt("Enter Your First Name");
+if(name != "aqib"){
+    alert("No Match")
+};
+
+// // Chapter 12 If... Else and Else if Statement
+
